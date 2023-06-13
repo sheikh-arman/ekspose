74474955b06ecba44111011f52236100486427c7
sheikh-arman <arman.cse4.bu@gmail.com>
2023-06-13 10:26:54 +0600

    added

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "/home/user/.kube/config", "location to your kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		// handle error
+		fmt.Printf("erorr %s building config from flags\n", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("error %s, getting inclusterconfig", err.Error())
+		}
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		// handle error
+		fmt.Printf("error %s, creating clientset\n", err.Error())
+	}
+	fmt.Println(clientset)
+}
