a88a3f2bfac004feffac96eac0aeb3f1550ff522
sheikh-arman <arman.cse4.bu@gmail.com>
2023-06-16 14:30:32 +0600

    added

--- controller.go
+++ controller.go
@@ -1,16 +1,116 @@
-type controller struct{
-	clientset kubernetes.Interface
-	depLister appslisters.DeploymentLister
-	depCacheSynced cache.InformerSynced
-	queue workqueue.RateLimitingInterface
+package main
 
+import (
+	"context"
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1
+	"k8s.io/apimachinery/pkg/util/wait"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
+	"k8s.io/client-go/kubernetes"
+	appslisters "k8s.io/client-go/listers/apps/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"time"
+)
+
+type controller struct {
+	clientset      kubernetes.Interface
+	depLister      appslisters.DeploymentLister
+	depCacheSynced cache.InformerSynced
+	queue          workqueue.RateLimitingInterface
 }
-func newController(clientset kubernetes.Interface, depInformer appsinformers.DeploymentInformer) *controller{
-	c:= &controller{
+
+func newController(clientset kubernetes.Interface, depInformer appsinformers.DeploymentInformer) *controller {
+	c := &controller{
+		clientset:      clientset,
+		depLister:      depInformer.Lister(),
+		depCacheSynced: depInformer.Informer().HasSynced,
+		queue:          workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "ekspose"),
+	}
+	depInformer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc: c.handleAdd,
+			AddFunc:    c.handleAdd,
 			DeleteFunc: c.handleDel,
 		},
-	}
+	)
 	return c
+}
+
+func (c *controller) run(ch <-chan struct{}) {
+	fmt.Println("Starting Controller")
+	if !cache.WaitForCacheSync(ch, c.depCacheSynced) {
+		fmt.Printf("Waiting for cache to be synced\n")
+	}
+	go wait.Until(c.worker, 1*time.Second, ch)
+
+	<-ch
+}
+
+func (c *controller) worker() {
+	for c.processItem() {
+
+	}
+}
+
+func (c *controller) processItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+
+	key, err := cache.MetaNamespaceKeyFunc(item)
+	if err != nil {
+		fmt.Printf("gettiong key from cache %s\n", err.Error())
+	}
+
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		fmt.Printf("splitting key into namespace and name %s\n", err.Error())
+	}
+	err = c.syncDeployment()
+	if err != nil {
+		fmt.Printf("syncing deployment %s\n", err.Error())
+		return false
+	}
+	return true
+}
+
+func (c *controller) syncDeployment(ns, name string) error {
+	ctx := context.Background()
+
+	dep, err := c.depLister.Deployments(ns).Get(name)
+	if err != nil {
+		fmt.Printf("getting deployment from lister %s\n", err.Error())
+	}
+
+	svc := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dep.Name,
+			Namespace: ns,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				corev1.ServicePort{
+					Name: "http",
+					Port: 80,
+				},
+			},
+		},
+	}
+	_, err := c.clientset.CoreV1(), ServiceS(ns).Create(ctx, &svc, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Printf("creating service %s\n", err.Error())
+	}
+	return nil
+}
+
+func (c *controller) handleAdd(obj interface{}) {
+	fmt.Println("add was called")
+	c.queue.Add(obj)
+}
+
+func (c *controller) handleDel(obj interface{}) {
+	fmt.Println("del Was called")
+	c.queue.Add(obj)
 }
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"time"
 )
 
 func main() {
@@ -22,5 +24,13 @@ func main() {
 	if err != nil {
 		fmt.Printf("error %s, creating clientset\n", err.Error())
 	}
-	fmt.Println(clientset)
+	ch := make(chan struct{})
+	informers := informers.NewSharedInformerFactory(clientset, 10*time.Minute)
+	c := newController(clientset, informers.Apps().V1().Deployments())
+	informers.Start(ch)
+	c.run(ch)
+	if err != nil {
+		fmt.Printf("getting informer factory %s\n", err.Error())
+	}
+	fmt.Println(informers)
 }
